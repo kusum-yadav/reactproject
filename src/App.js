998e4292b1fbc34c3f02b4ae494b3f947ad21c6e
kusum-yadav <kusumyadavkota@gmail.com>
2021-03-22 09:06:55 +0530

    Inital Commit of ReactProject

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class App extends React.Component {  
+
+  render() {
+    return (
+      <div id="App">
+        <div className="select-container">
+          Hello There!!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
